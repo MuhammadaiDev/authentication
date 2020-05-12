c5f707f44f737856e05a5633b8a63f38a0efadec
Muhammad Arshad <arshadaidev@gmail.com>
2020-05-12 10:45:12 -0700

    used bcrypt package

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express()
 
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
 });
 //taking secret from enviornment file
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] }); <--this wass a encryption method for mongoose-encryption
 const User = new mongoose.model("User", userSchema);
 
 app.get('/', function(req, res){
@@ -36,17 +37,22 @@ app.get('/register', function(req, res){
 
 //posting resgiter page
 app.post("/register", function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
     });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
-    });
+
 });
 
 app.post('/login', function(req, res){
@@ -57,8 +63,14 @@ app.post('/login', function(req, res){
         if(err){
             console.log("error")
         }else{
-            if(password === foundUser.password){
-                res.render("secrets")
+            if(foundUser){
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result===true){
+                        res.render("secrets")  
+                    }
+                });
+                   
+              
             }
         }
     });
